6814b0543d8d9158f88133ca9b3afdfde765d430
diegotalejandro <diego.torreblanca2013@gmail.com>
2020-06-13 23:04:08 -0400

    Se completo Huffmann

---
+++ Huffman/entropia.py
@@ -0,0 +1,111 @@
+import numpy as np
+from scipy.stats import entropy
+from math import log, e
+import pandas as pd
+
+import timeit
+
+
+import math
+from collections import Counter
+
+def entropy(labels):
+    """ Computes entropy of 0-1 vector. """
+    n_labels = len(labels)
+
+    if n_labels <= 1:
+        return 0
+
+    counts = np.bincount(labels)
+    probs = counts[np.nonzero(counts)] / n_labels
+    n_classes = len(probs)
+
+    if n_classes <= 1:
+        return 0
+    return - np.sum(probs * np.log(probs)) / np.log(n_classes)
+
+
+def entropy2(labels, base=None):
+  """ Computes entropy of label distribution. """
+
+  n_labels = len(labels)
+
+  if n_labels <= 1:
+    return 0
+
+  value, counts = np.unique(labels, return_counts=True)
+  probs = counts / n_labels
+  n_classes = np.count_nonzero(probs)
+
+  if n_classes <= 1:
+    return 0
+
+  ent = 0.
+
+  # Compute entropy
+  base = e if base is None else base
+  for i in probs:
+    ent -= i * log(i, base)
+
+  return ent
+
+
+def eta(data, unit='natural'):
+       base = {
+           'shannon': 2.,
+           'natural': math.exp(1),
+           'hartley': 10.
+       }
+
+       if len(data) <= 1:
+           return 0
+
+       counts = Counter()
+
+       for d in data:
+           counts[d] += 1
+
+       probs = [float(c) / len(data) for c in counts.values()]
+       probs = [p for p in probs if p > 0.]
+
+       ent = 0
+
+       for p in probs:
+           if p > 0.:
+               ent -= p * math.log(p, base[unit])
+
+       return ent
+
+strings = ["zkncdmqafvewfqqwvftr",
+           "bydimeiddjncrvwahrxk",
+           "ejunnkwykphqeftguetc",
+           "wgnedmhdqcnthmxmaeih",
+           "ncucwiffwtzpzxnjryif",
+           "qyfrhgzdcirigmjviygq",
+           "jnmmqinxfvpkhnaacbfj",
+           "frnbmhxmjurqahxnuvqd",
+           "xjjcqbiqcwvrdqimrntt",
+           "dzvceqehcnkwpikmjizr",
+           "pazfvdujarttiqhteqeu",
+           "ptiuacatembkikmnjcpg",
+           "ubquwvrgfpmybdgyqxjt",
+           "crmbmpcxgunzdhgappxf",
+           "cadvcnpurjegkhbanhqe",
+           "xmjphxjgtkbqcpztyzex",
+           "cafddrnmbrkfefwfnbij",
+           "bbcdgpewhbxczptxxung",
+           "cczzetgbkkxukbgbaxum",
+           "iinqgcwfjuqpgiizrepn",
+           "mrububyyfnyumxmiquxg",
+           "qfbqvfzbmakwadcpjawe",
+           "ihuuzahpymjwuwecgvub",
+           "ekccrwnpqmmtuxeqmdty",
+           "xigjjpjivjtetepexdiy",
+           "akkgzttvqqmkzfxvzxhr",
+           "upwgqukarcvmyiyjnthg",
+           "zrqhnhdxkwxagpdirnhw",
+           "dzkxapxjabtmirggugav",
+           "ktueybhjxcgpymazdqkb"]
+print(entropy2(strings))
+print(eta(strings))
+print(eta(['mary', 'had', 'a', 'little', 'lamb']))
--- Huffman/laboratorio_huffman.py
+++ Huffman/laboratorio_huffman.py
@@ -4,44 +4,16 @@ import argparse
 from HuffmanCoding import HuffmanCoding
 import base64
 from time import process_time
-string_entrada = [
-    zkncdmqafvewfqqwvftr,
-    bydimeiddjncrvwahrxk,
-    ejunnkwykphqeftguetc,
-    wgnedmhdqcnthmxmaeih,
-    ncucwiffwtzpzxnjryif,
-    qyfrhgzdcirigmjviygq,
-    jnmmqinxfvpkhnaacbfj,
-    frnbmhxmjurqahxnuvqd,
-    xjjcqbiqcwvrdqimrntt,
-    dzvceqehcnkwpikmjizr,
-    pazfvdujarttiqhteqeu,
-    ptiuacatembkikmnjcpg,
-    ubquwvrgfpmybdgyqxjt,
-    crmbmpcxgunzdhgappxf,
-    cadvcnpurjegkhbanhqe,
-    xmjphxjgtkbqcpztyzex,
-    cafddrnmbrkfefwfnbij,
-    bbcdgpewhbxczptxxung,
-    cczzetgbkkxukbgbaxum,
-    iinqgcwfjuqpgiizrepn,
-    mrububyyfnyumxmiquxg,
-    qfbqvfzbmakwadcpjawe,
-    ihuuzahpymjwuwecgvub,
-    ekccrwnpqmmtuxeqmdty,
-    xigjjpjivjtetepexdiy,
-    akkgzttvqqmkzfxvzxhr,
-    upwgqukarcvmyiyjnthg,
-    zrqhnhdxkwxagpdirnhw,
-    dzkxapxjabtmirggugav,
-    ktueybhjxcgpymazdqkb]
+
+
 def file_to_string(path):
     string = ""
     with open(path, 'rb') as file:
-        content_file = file.read()        
+        content_file = file.read()
         #base64_content_file = base64.b64encode(content_file)
         #string_img = string_img + base64_content_file.decode("utf-8")
     return content_file
+
 
 def base64_to_img(path, img_name):
     with open(path, 'rb') as file_img:
@@ -49,6 +21,7 @@ def base64_to_img(path, img_name):
     content_file = base64.b64decode(string_img)
     with open(img_name, 'wb') as f:
         f.write(content_file)
+
 
 def write_string_in_a_file(string, file_name):
     f = open(file_name, 'w')
@@ -60,13 +33,16 @@ def compress_string(file_name):
     h = HuffmanCoding(file_name)
     h.compress()
     #output_path = h.compress()
-    #h.decompress(output_path)
+    # h.decompress(output_path)
+
 
 time_start = process_time()
-#--------------------------------------------------------
-file_name = sys.argv[1]
-if os.path.exists(file_name):    
-    compress_string(file_name)
-#--------------------------------------------------------
+# --------------------------------------------------------
+#file_name = sys.argv[1]
+# if os.path.exists(file_name):
+for i in range(1, 31):
+    compress_string(str(i) + ".txt")    
+# --------------------------------------------------------
 time_stop = process_time()
-print("Tiempo de duración del programa: ", time_stop-time_start, " segundos.")
+print("Tiempo de duración de la compresión: ",
+      time_stop-time_start, " segundos.")
